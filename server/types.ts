3dd1c2d3a0d22bc4c8a6d1ef93ac1b08bf718899
Mohamed El Tawargy <tawargy.dev@gmail.com>
2023-05-04 15:18:11 +0300

    add schemas type

---
+++ server/types.ts
@@ -0,0 +1,36 @@
+
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  password: string;
+}
+
+export interface Post {
+  id: string;
+  title: string;
+  url: string;
+  userId: string;
+  postedAt: number;
+  likes?:number;
+  dislike?:number;
+  userLike?:boolean;
+  userDislike?:boolean;
+  comments?:number;
+}
+export interface Like {
+  userId: string;
+  postId: string;
+}
+export interface Dislike extends Like{}
+
+
+export interface Comment {
+  id: string;
+  userId: string;
+  postId: string;
+  comment: string;
+  postedAt: number;
+}
